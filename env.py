94a8850d83e83bb33904d428f47642296377ed82
liambaker93 <3c2mrjqh@students.codeinstitute.net>
2025-07-20 20:56:03 +0100

    add extra forms to Post class

--- env.py
+++ env.py
@@ -1,4 +1,4 @@
-import os 
+import os
 
 os.environ.setdefault(
     "DATABASE_URL", "postgresql://neondb_owner:npg_gn2Xh8ytQiFY@ep-wispy-star-a2lcoa9d.eu-central-1.aws.neon.tech/chef_usher_apple_271554")
