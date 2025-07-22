36da3dedb107847b0fee038d5449e2ec2df20b07
liambaker93 <3c2mrjqh@students.codeinstitute.net>
2025-07-22 21:44:40 +0100

    add PostList class into views within blog. added html front to the oage.

--- blog/views.py
+++ blog/views.py
@@ -6,6 +6,6 @@ from .models import Post
 
 
 class PostList(generic.ListView):
-    queryset = Post.objects.all()
+    queryset = Post.objects.filter(author=2)
     template_name = "post_list.html"
 
