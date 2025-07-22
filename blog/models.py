5919a8243d1b415c4697ca75fbfb75b5a2cc6c45
liambaker93 <3c2mrjqh@students.codeinstitute.net>
2025-07-22 20:24:13 +0100

    fix a typo in comment class

--- blog/models.py
+++ blog/models.py
@@ -15,7 +15,7 @@ class Post(models.Model):
     updated_on = models.DateTimeField(auto_now=True)
 
 
-class Comment(models.model):
+class Comment(models.Model):
     post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="comments")
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="blog_posts")
     body = models.TextField()
