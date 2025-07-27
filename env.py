0221b109536c6ebe5320ca5fb1d39e7c8b0faf6c
liambaker93 <3c2mrjqh@students.codeinstitute.net>
2025-07-27 12:01:58 +0100

    add cloudinary support

--- env.py
+++ env.py
@@ -4,3 +4,5 @@ os.environ.setdefault(
     "DATABASE_URL", "postgresql://neondb_owner:npg_gn2Xh8ytQiFY@ep-wispy-star-a2lcoa9d.eu-central-1.aws.neon.tech/chef_usher_apple_271554")
 
 os.environ.setdefault("SECRET_KEY", "!2f1&n^xoqua=n9!sas4-y+m$7lp!)b^d#^o3l^t96&&v-+ad1")
+
+os.environ.setdefault("CLOUDINARY_URL", "cloudinary://<your_api_key>:<your_api_secret>@dgwkg4zwp")
